be1c416cac277c7805d20433af0529c3c6868696
shaukat1388 <shaukat1388@gmail.com>
2025-12-27 11:52:36 -0600

    mera commit

---
+++ batman-user/main.tf
@@ -0,0 +1,10 @@
+resource "azuread_user" "batman" {
+  user_principal_name = "batman@${data.azuread_domains.tenant.domains[0].domain_name}"
+  display_name        = "Batman"
+  mail_nickname       = "batman"
+
+  password = "P@ssw0rd-Batman-123!"
+
+  force_password_change = true
+  account_enabled       = true
+}
