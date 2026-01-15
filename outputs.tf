d033cb1c2f4154e05ae31af644c3003a1bc9be07
shaukat1388 <shaukat1388@gmail.com>
2026-01-15 12:32:16 -0600

    Add two-users terraform folder

---
+++ outputs.tf
@@ -0,0 +1,3 @@
+output "created_users" {
+  value = module.aad_users.user_upns
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "salman_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "amir_password" {
+  type      = string
+  sensitive = true
+}
